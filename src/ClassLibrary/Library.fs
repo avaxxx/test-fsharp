45c9327a7b02352abf3523615b934e05b3de8ee6
Pavol Decky <avaxxx@gmail.com>
2020-03-02 13:28:40 +0100

    Upgrade to .NET Core 3.1

--- src/ClassLibrary/Library.fs
+++ src/ClassLibrary/Library.fs
@@ -1,5 +1,42 @@
 ﻿namespace ClassLibrary
+open System.Runtime.Loader
+open System
 
+type Person = { FirstName:string; LastName:string }
+
+type PaymentType =
+    | Cash
+    | Check of checkNumber:int
+    | CreditCard of cardNumber:string * expiration:string
 module Say =
+
+
     let hello name =
         printfn "Hello %s" name
+    
+    let pavol = {FirstName = "Pavol"; LastName = "Decky"}
+
+    let peter = {pavol with FirstName = "Peter"}
+
+    let cash = Cash
+    let check = Check 150
+
+
+    let tryParseDateTime (input:string) =
+        let (success, result) = DateTime.TryParse input
+        if success then Some result
+        else None
+    
+    let tryParseDateTime2 (input: string) =
+        match DateTime.TryParse input with
+        | true, result -> Some result
+        | _ -> None
+
+    let website = System.Uri "http://www.sme.sk"
+
+module TryScope =
+    let globalvar = 10
+    let fn input = 
+        let output = input + globalvar
+        output
+    let test = globalvar
--- src/MainConsole/Program.fs
+++ src/MainConsole/Program.fs
@@ -2,5 +2,8 @@
 
 [<EntryPoint>]
 let main argv =
+    printfn "test"
     printfn "Hello World from F#!"
+    let website = new System.Uri("http://www.sme.sk")
+    printfn "Absolute path %s" website.AbsolutePath
     0 // return an integer exit code
